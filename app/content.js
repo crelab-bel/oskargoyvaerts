366b989ef64fc19a84cbfd2470acf3f09a4cbd02
crelab-bel <168651978+crelab-bel@users.noreply.github.com>
2024-05-02 20:00:41 +0200

    Add Base64 encoded file

--- app/content.js
+++ app/content.js
@@ -1,4 +1,4 @@
 export const mainHero = {
-    h1: 'Plaats een SEO vriendelijke Hero tekst hier',
-    paragraaf: 'Plaats een SEO vriendelijke text die de H1 text ondersteunt hier',
+    h1: 'Ontdek Topkwaliteit Fitnessapparatuur voor Elke Training',
+    paragraaf: 'Verhoog je kracht en bouw spieren op met onze duurzame en betrouwbare fitnessapparaten, ontworpen voor zowel beginners als professionele atleten.',
 };
